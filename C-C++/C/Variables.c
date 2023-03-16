ad29337fcdd395e70a6cf027ce16c49e07aab845
Rhys <124939732+Rhys1502@users.noreply.github.com>
2023-03-16 05:16:37 +0000

    Add files via upload

---
+++ C-C++/C/Variables.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    // integer variables
+    int x = 10;
+    int y = 20;
+
+    // floating-point variables
+    float f1 = 1.23;
+    double f2 = 4.56;
+
+    // character variables
+    char c1 = 'a';
+    char c2 = 65; // ASCII code for 'A'
+
+    // boolean variable
+    int b = 1;
+
+    // display variable values
+    printf("x = %d, y = %d\n", x, y);
+    printf("f1 = %f, f2 = %lf\n", f1, f2);
+    printf("c1 = %c, c2 = %c\n", c1, c2);
+    printf("b = %d\n", b);
+
+    return 0;
+}
